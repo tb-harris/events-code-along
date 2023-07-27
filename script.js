0abd9fb661f2528609c476dbac451b3aae0435e3
tb-harris <tbharris@ucdavis.edu>
2023-07-27 17:38:56 -0400

    minor changes

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+/* CLICK EVENT */
 // get div reference
 let divRef = document.querySelector("div");
 
@@ -22,29 +23,38 @@ buttonRef.onclick = function() {
     }
 }
 
-/* TOGGLING DISPLAY */
+// (optional)
+// style property does not automatically what's in style.css
+// so if we don't include this line, color will stay orange
+// on first press
+divRef.style.backgroundColor = "orange";
 
-let displayButton = document.getElementById("display");
+/*************************************/
+
+/* TOGGLING DISPLAY */
+// get refernece to show/hide button
+let displayButton = document.getElementById("display-btn");
 
 // set up an onclick function for visibilityButton
 displayButton.onclick = function() {
     // whenever you click on the button,
     // if the box is visible, hide it 
-    if (divRef.style.display == "block") {
+    if (divRef.style.display != "none") {
         divRef.style.display = "none";
+        // update button text
         displayButton.innerHTML = "show";
     }
 
     // if the box is hidden, show it
     else {
         divRef.style.display = "block";
+        // update button text
         displayButton.innerHTML = "hide";
     }
 }
+/*************************************/
 
-
-/* MOUSE OVER AND MOUSE OUT */
-
+/* MOUSE OVER AND MOUSE OUT EVENTS */
 // onmouseover
 divRef.onmouseover = function() {
     // console.log("The mouse is on the div");
@@ -52,15 +62,14 @@ divRef.onmouseover = function() {
     divRef.innerHTML = "Ouch!";
 
     // decrease the opacity
-    divRef.style.opacity = "0.8";
+    divRef.style.opacity = "0.6";
 }
 
 // onmouseout
 divRef.onmouseout = function() {
-    divRef.style.backgroundColor = "orange";
     divRef.innerHTML = "The mouse is not on me anymore";
 
     // increase the opacity back to 1
     divRef.style.opacity = "1";
 }
-        
+/*************************************/
